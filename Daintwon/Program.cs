3a85c06b42eb332915e56a90dbfdd87cf027b126
MatiasSanPietro <matisanpietro98@gmail.com>
2024-03-07 14:45:38 -0300

    Add repository base & configure JWT

--- Daintwon/Program.cs
+++ Daintwon/Program.cs
@@ -1,5 +1,8 @@
 using Daintwon.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +15,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers().AddJsonOptions(x =>
 x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+
 // Repositories
+
+// JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("ClientOnly", policy => policy.RequireClaim("Client"));
 
 var app = builder.Build();
 
@@ -32,6 +56,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapControllers();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ Daintwon/Repositories/Implementations/RepositoryBase.cs
@@ -0,0 +1,54 @@
+﻿using Daintwon.Models;
+using Daintwon.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Daintwon.Repositories.Implementations
+{
+    public class RepositoryBase<T>(DaintwonContext repositoryContext) : IRepositoryBase<T> where T : class
+    {
+        protected DaintwonContext RepositoryContext { get; set; } = repositoryContext;
+
+        public IQueryable<T> FindAll()
+        {
+            return RepositoryContext.Set<T>().AsNoTrackingWithIdentityResolution();
+        }
+
+        public IQueryable<T> FindAll(Func<IQueryable<T>, IIncludableQueryable<T, object>> includes = null)
+        {
+            IQueryable<T> queryable = RepositoryContext.Set<T>();
+
+            if (includes != null)
+            {
+                queryable = includes(queryable);
+            }
+            return queryable.AsNoTrackingWithIdentityResolution();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return RepositoryContext.Set<T>().Where(expression).AsNoTrackingWithIdentityResolution();
+        }
+
+        public void Create(T entity)
+        {
+            RepositoryContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            RepositoryContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            RepositoryContext.Set<T>().Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            RepositoryContext.SaveChanges();
+        }
+    }
+}
